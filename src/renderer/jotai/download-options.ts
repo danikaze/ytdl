cfef957b910acea8f701fc3223a12715ff8e58fd
danikaze <danikaze@gmail.com>
2022-11-05 13:45:24 +0900

    use cached functions in hooks

--- src/renderer/jotai/download-options.ts
+++ src/renderer/jotai/download-options.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { atom, useAtom } from 'jotai';
 import { DownloadPostProcessOptions } from '@interfaces/download';
 import type { DownloadType } from '@interfaces/settings';
@@ -59,153 +60,181 @@ export function useDownloadOptions() {
   const [modal, setModal] = useAtom(rawModal);
   const { getSetting, updateSetting } = useSettings();
 
-  async function openModal(url: string) {
-    const initialMetadata = undefined;
-    const initialFilename = '[title]';
-    const initialOutputFile = replaceMetadata(initialFilename, initialMetadata);
+  const openModal = useCallback(
+    async (url: string): Promise<void> => {
+      const initialMetadata = undefined;
+      const initialFilename = '[title]';
+      const initialOutputFile = replaceMetadata(
+        initialFilename,
+        initialMetadata
+      );
 
-    setModal({
-      url,
-      show: true,
-      filename: initialFilename,
-      isValidFilename: isValidFilename(initialOutputFile),
-      metadata: initialMetadata,
-      downloadType: getSetting('downloads.downloadType', true),
-      downloadOptions: {
-        outputFile: initialOutputFile,
-        outputFolder: getSetting('downloads.downloadFolder', true),
-      },
-      downloadAudioOptions: {
-        format: getSetting('downloads.audio.audioFormat', true),
-      },
-      downloadVideoOptions: {
-        format: getSetting('downloads.video.videoFormat', true),
-      },
-      postProcessOptions: {},
-    });
-
-    try {
-      const metadata = await window.ytdl.fetchMetadata(url);
-      setModal((currentModal) => {
-        const finalFilename = replaceMetadata(currentModal.filename, metadata);
-        return {
-          ...currentModal,
-          url,
-          metadata,
-          isValidFilename: isValidFilename(finalFilename),
-          downloadOptions: {
-            ...currentModal.downloadOptions,
-            outputFile: finalFilename,
-          },
-        };
+      setModal({
+        url,
+        show: true,
+        filename: initialFilename,
+        isValidFilename: isValidFilename(initialOutputFile),
+        metadata: initialMetadata,
+        downloadType: getSetting('downloads.downloadType', true),
+        downloadOptions: {
+          outputFile: initialOutputFile,
+          outputFolder: getSetting('downloads.downloadFolder', true),
+        },
+        downloadAudioOptions: {
+          format: getSetting('downloads.audio.audioFormat', true),
+        },
+        downloadVideoOptions: {
+          format: getSetting('downloads.video.videoFormat', true),
+        },
+        postProcessOptions: {},
       });
-    } catch (error) {
-      setModal((currentModal) => ({
-        ...currentModal,
-        error: String(error),
-      }));
-    }
-  }
 
-  function closeModal() {
+      try {
+        const metadata = await window.ytdl.fetchMetadata(url);
+        setModal((currentModal) => {
+          const finalFilename = replaceMetadata(
+            currentModal.filename,
+            metadata
+          );
+          return {
+            ...currentModal,
+            url,
+            metadata,
+            isValidFilename: isValidFilename(finalFilename),
+            downloadOptions: {
+              ...currentModal.downloadOptions,
+              outputFile: finalFilename,
+            },
+          };
+        });
+      } catch (error) {
+        setModal((currentModal) => ({
+          ...currentModal,
+          error: String(error),
+        }));
+      }
+    },
+    [setModal, getSetting]
+  );
+
+  const closeModal = useCallback(() => {
     setModal((currentModal) => ({
       ...currentModal,
       url: modal.url,
       show: false,
     }));
-  }
+  }, [setModal, modal.url]);
 
-  function selectDownloadOutputFile(filename: string) {
-    setModal((currentModal) => {
-      const outputFile = replaceMetadata(filename, modal.metadata);
-      return {
+  const selectDownloadOutputFile = useCallback(
+    (filename: string) => {
+      setModal((currentModal) => {
+        const outputFile = replaceMetadata(filename, modal.metadata);
+        return {
+          ...currentModal,
+          filename,
+          isValidFilename: isValidFilename(outputFile),
+          downloadOptions: {
+            ...currentModal.downloadOptions,
+            outputFile,
+          },
+        };
+      });
+    },
+    [setModal, modal.metadata]
+  );
+
+  const selectDownloadOutputFolder = useCallback(
+    (folder: string) => {
+      updateSetting('last.downloads.downloadFolder', folder);
+      setModal((currentModal) => ({
         ...currentModal,
-        filename,
-        isValidFilename: isValidFilename(outputFile),
         downloadOptions: {
           ...currentModal.downloadOptions,
-          outputFile,
+          outputFolder: folder,
         },
-      };
-    });
-  }
+      }));
+    },
+    [setModal, updateSetting]
+  );
 
-  function selectDownloadOutputFolder(folder: string) {
-    updateSetting('last.downloads.downloadFolder', folder);
-    setModal((currentModal) => ({
-      ...currentModal,
-      downloadOptions: {
-        ...currentModal.downloadOptions,
-        outputFolder: folder,
-      },
-    }));
-  }
+  const selectDownloadType = useCallback(
+    (type: DownloadType) => {
+      updateSetting('last.downloads.downloadType', type);
+      setModal((currentModal) => ({
+        ...currentModal,
+        downloadType: type,
+      }));
+    },
+    [setModal, updateSetting]
+  );
 
-  function selectDownloadType(type: DownloadType) {
-    updateSetting('last.downloads.downloadType', type);
-    setModal((currentModal) => ({
-      ...currentModal,
-      downloadType: type,
-    }));
-  }
-
-  function updateDownloadAudioOptions<K extends keyof YoutubeDlAudioOptions>(
-    key: K,
-    value: YoutubeDlAudioOptions[K]
-  ) {
-    // update last values if needed
-    if (value !== undefined) {
-      if (key === 'format') {
-        updateSetting(
-          'last.downloads.audio.audioFormat',
-          value as Exclude<YoutubeDlAudioOptions['format'], undefined>
-        );
+  const updateDownloadAudioOptions = useCallback(
+    <K extends keyof YoutubeDlAudioOptions>(
+      key: K,
+      value: YoutubeDlAudioOptions[K]
+    ) => {
+      // update last values if needed
+      if (value !== undefined) {
+        if (key === 'format') {
+          updateSetting(
+            'last.downloads.audio.audioFormat',
+            value as Exclude<YoutubeDlAudioOptions['format'], undefined>
+          );
+        }
       }
-    }
-    // update the modal
-    setModal((currentModal) => ({
-      ...currentModal,
-      downloadAudioOptions: {
-        ...currentModal.downloadAudioOptions,
-        [key]: value,
-      },
-    }));
-  }
+      // update the modal
+      setModal((currentModal) => ({
+        ...currentModal,
+        downloadAudioOptions: {
+          ...currentModal.downloadAudioOptions,
+          [key]: value,
+        },
+      }));
+    },
+    [setModal, updateSetting]
+  );
 
-  function updateDownloadVideoOptions<K extends keyof YoutubeDlVideoOptions>(
-    key: K,
-    value: YoutubeDlVideoOptions[K]
-  ) {
-    // update last values if needed
-    if (value !== undefined) {
-      if (key === 'format') {
-        updateSetting(
-          'last.downloads.video.videoFormat',
-          value as Exclude<YoutubeDlVideoOptions['format'], undefined>
-        );
+  const updateDownloadVideoOptions = useCallback(
+    <K extends keyof YoutubeDlVideoOptions>(
+      key: K,
+      value: YoutubeDlVideoOptions[K]
+    ) => {
+      // update last values if needed
+      if (value !== undefined) {
+        if (key === 'format') {
+          updateSetting(
+            'last.downloads.video.videoFormat',
+            value as Exclude<YoutubeDlVideoOptions['format'], undefined>
+          );
+        }
       }
-    }
-    // update the modal
-    setModal((currentModal) => ({
-      ...currentModal,
-      downloadVideoOptions: {
-        ...currentModal.downloadVideoOptions,
-        [key]: value,
-      },
-    }));
-  }
+      // update the modal
+      setModal((currentModal) => ({
+        ...currentModal,
+        downloadVideoOptions: {
+          ...currentModal.downloadVideoOptions,
+          [key]: value,
+        },
+      }));
+    },
+    [setModal, updateSetting]
+  );
 
-  function updateDownloadPostProcessOptions<
-    K extends keyof DownloadPostProcessOptions
-  >(key: K, value: Partial<DownloadPostProcessOptions[K]>) {
-    setModal((currentModal) => ({
-      ...currentModal,
-      postProcessOptions: {
-        ...currentModal.postProcessOptions,
-        [key]: value,
-      },
-    }));
-  }
+  const updateDownloadPostProcessOptions = useCallback(
+    <K extends keyof DownloadPostProcessOptions>(
+      key: K,
+      value: Partial<DownloadPostProcessOptions[K]>
+    ): void => {
+      setModal((currentModal) => ({
+        ...currentModal,
+        postProcessOptions: {
+          ...currentModal.postProcessOptions,
+          [key]: value,
+        },
+      }));
+    },
+    [setModal]
+  );
 
   return {
     open: openModal,

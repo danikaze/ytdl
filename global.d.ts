82f9da0ba0c83f17f51e70263d0de1b63236efa0
danikaze <danikaze@gmail.com>
2022-11-15 22:21:32 +0900

    FileSyncObject class

--- global.d.ts
+++ global.d.ts
@@ -6,11 +6,19 @@ type DeepPartial<T> = T extends object
 
 type DeepReadonly<T> = T extends object
   ? {
-      [P in keyof T]?: DeepReadonly<T[P]>;
+      [P in keyof T]: DeepReadonly<T[P]>;
     }
   : Readonly<T>;
 
-type Nullable<T extends {}> = { [key in keyof T]: T[key] | null };
+type Nullable<T extends {}> = {
+  [K in keyof T]: T[K] | null;
+};
+
+type DeepNullable<T> = T extends object
+  ? {
+      [K in keyof T]: DeepNullable<T[K]> | null;
+    }
+  : T | null;
 
 type KeysOfWithValue<O extends {}, T> = Exclude<
   {

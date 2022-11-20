0e0347cde8640eeb029a6146124aee0f6e26e2ae
danikaze <danikaze@gmail.com>
2022-11-20 21:23:44 +0900

    Catalogue on disk to persist data

---
+++ src/main/catalogue/db.ts
@@ -0,0 +1,109 @@
+import { join } from 'path';
+import { nanoid } from '../../npm/nanoid';
+import { Download, DownloadState } from '../../interfaces/download';
+import {
+  createFileSyncObject,
+  FileSyncObject,
+} from '../utils/file-sync-object';
+
+export interface CatalogueDbOptions {
+  path: string;
+}
+
+interface CatalogueDbData {
+  version: number;
+  createdOn: string;
+  updatedOn: string;
+  downloads: Record<Download['id'], Download>;
+}
+
+export class CatalogueDb {
+  private static VERSION = 1;
+
+  private path: string;
+
+  private db: FileSyncObject<CatalogueDbData>;
+
+  constructor({ path }: CatalogueDbOptions) {
+    this.path = join(path, 'db');
+    this.db = createFileSyncObject(this.path, {
+      prettify: true,
+      initialData: {
+        version: CatalogueDb.VERSION,
+        createdOn: new Date().toISOString(),
+        updatedOn: new Date().toISOString(),
+        downloads: {},
+      },
+      beforeSync: (data) => {
+        data.updatedOn = new Date().toISOString();
+      },
+    });
+  }
+
+  public async upgrade(): Promise<void> {
+    const { version } = this.db.data;
+
+    // init version
+    if (!this.db.data.version) {
+      this.db.data.version = CatalogueDb.VERSION;
+    }
+    if (!this.db.data.downloads) {
+      this.db.data.downloads = {};
+    }
+
+    if (this.db.data.version === CatalogueDb.VERSION) return;
+
+    console.log(
+      `Updating CatalogueDB from ${version} to ${CatalogueDb.VERSION}`
+    );
+
+    this.db.update({ version: CatalogueDb.VERSION });
+  }
+
+  public getDownload(id: Download['id']): Download | undefined {
+    return this.db.data.downloads[id];
+  }
+
+  public addDownload(
+    download: Pick<Download, 'url' | 'postProcess' | 'format'>
+  ): Download['id'] {
+    const id = nanoid();
+    this.db.update({
+      downloads: {
+        [id]: {
+          id,
+          downloadPctg: 0,
+          temporalFile: '',
+          state: DownloadState.INITIALIZATING,
+          ...download,
+        },
+      },
+    });
+
+    return id;
+  }
+
+  public updateDownload(
+    download: Pick<Download, 'id'> & Nullable<Partial<Omit<Download, 'id'>>>
+  ): void {
+    const { id } = download;
+    if (!id) {
+      console.log(`Tried to update an unknown download with id: ${id}`);
+      return;
+    }
+
+    this.db.update({
+      downloads: {
+        [id]: {
+          ...this.db.data.downloads[id],
+          ...download,
+        },
+      },
+    });
+  }
+
+  public removeDownload(id: Download['id']): void {
+    delete this.db.data.downloads[id];
+    this.db.update({});
+  }
+}
---
+++ src/main/catalogue/index.ts
@@ -0,0 +1,67 @@
+import { app } from 'electron';
+import { existsSync } from 'fs';
+import { join } from 'path';
+import { sync as mkdirpSync } from 'mkdirp';
+import { Download } from '../../interfaces/download';
+import { CatalogueDb } from './db';
+
+export interface CatalogueOptions {
+  path?: string;
+}
+
+export class Catalogue {
+  private path: string;
+
+  private db!: CatalogueDb;
+
+  constructor(options?: CatalogueOptions) {
+    const { path } = {
+      path: join(app.getPath('userData'), 'catalogue'),
+      ...options,
+    };
+    this.path = path;
+  }
+
+  // eslint-disable-next-line consistent-return
+  public async open(): Promise<void> {
+    if (!existsSync(this.path)) {
+      return this.createLibrary();
+    }
+
+    console.log(`Opening catalogue from ${this.path}`);
+    this.db = new CatalogueDb({ path: this.path });
+
+    await this.upgrade();
+  }
+
+  public getDownload(id: Download['id']): Download | undefined {
+    return this.db.getDownload(id);
+  }
+
+  public addDownload(
+    download: Pick<Download, 'url' | 'postProcess' | 'format'>
+  ): Download['id'] {
+    return this.db.addDownload(download);
+  }
+
+  public updateDownload(
+    download: Pick<Download, 'id'> & Nullable<Partial<Omit<Download, 'id'>>>
+  ): void {
+    this.db.updateDownload(download);
+  }
+
+  public removeDownload(id: Download['id']): void {
+    this.db.removeDownload(id);
+  }
+
+  private async createLibrary(): Promise<void> {
+    console.log(`Creating new catalogue in ${this.path}`);
+
+    mkdirpSync(this.path);
+    this.db = new CatalogueDb({ path: this.path });
+  }
+
+  private async upgrade(): Promise<void> {
+    await this.db!.upgrade();
+  }
+}
